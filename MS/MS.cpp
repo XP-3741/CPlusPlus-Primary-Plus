696f477b067f10e60771b514a83ca3632e53b7f6
X_P <2325968853@qq.com>
2022-02-13 18:48:56 +0800

    add

--- MS/MS.cpp
+++ MS/MS.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 #include<string>
-//#include <algorithm> 
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-//using std::next_permutation;
-//using std::prev_permutation;
 
 #define ARRAY_SIZE 4
 int input[ARRAY_SIZE][ARRAY_SIZE];
@@ -16,29 +13,65 @@ struct RESULT
 {
 	int i;
 	int j;
-	int times = -1;
+	int times = 6666;
 };
 
-RESULT DFS(int i, int j,int times)
+void reset_input(int index)
 {
-	times++;
-	if (target[i][j] == 1) {
+	for (int i = 0; i < ARRAY_SIZE; i++)
+		for (int j = 0; j < ARRAY_SIZE; j++)
+			if (input[i][j] == index)
+				input[i][j] = 0;
+}
+
+RESULT DFS(int i, int j,int times,int MODE,int index)
+{
+	if (MODE == 1 && target[i][j] == 1) {
+		times++;
 		RESULT result = { i,j,times };
 		return result;
 	}
+	else if (MODE == 0 && times!=-1 && target[i][j] == 1) {
+		times++;
+		RESULT result = { i,j,times };
+		return result;
+	}
+	times++;
 	RESULT results[4];
-	if (i - 1 >= 0 && input[i - 1][j] != 1)	results[0] = DFS(i - 1, j, times);
-	if (i + 1 < ARRAY_SIZE && input[i + 1][j] != 1)	results[1] = DFS(i + 1, j, times);
-	if (j - 1 >= 0 && input[i][j - 1] != 1)	results[2] = DFS(i, j - 1, times);
-	if (i + 1 < ARRAY_SIZE && input[i][j + 1] != 1)	results[3] = DFS(i, j + 1, times);
+	if (i - 1 >= 0 && input[i - 1][j] == 0) {
+		input[i][j] = index;
+		results[0] = DFS(i - 1, j, times, MODE, index+1);
+		reset_input(index + 1);
+	}
+	if (i + 1 < ARRAY_SIZE && input[i + 1][j] == 0) {
+		input[i][j] = index;
+		results[1] = DFS(i + 1, j, times, MODE, index+1);
+		reset_input(index + 1);
+	}
+	if (j - 1 >= 0 && input[i][j - 1] == 0) {
+		input[i][j] = index;
+		results[2] = DFS(i, j - 1, times, MODE, index+1);
+		reset_input(index + 1);
+	}
+	if (j + 1 < ARRAY_SIZE && input[i][j + 1] == 0) {
+		input[i][j] = index;
+		results[3] = DFS(i, j + 1, times, MODE, index+1);
+		reset_input(index + 1);
+	}
 	int result_index = 0;
 	for (int index = 0; index < 4; index++)
-		if (results[index].times != -1 && results[result_index].times > results[index].times)
+		if (results[result_index].times > results[index].times)
 			result_index = index;
-
-	input[results[result_index].i][results[result_index].j] = 1;
-	input[i][j] = 0;
-	target[results[result_index].i][results[result_index].j] = 2;
+	if (MODE == 1) {
+		input[results[result_index].i][results[result_index].j] = 2;
+		input[i][j] = 0;
+	}
+	else {
+		input[results[result_index].i][results[result_index].j] = 0;
+		input[i][j] = 2;
+	}
+	if (results[result_index].times == 6666)
+		results[result_index].times = 0;
 	return results[result_index];
 }
 
@@ -57,21 +90,37 @@ int main()
 			target[i][j] = input_string[j] - '0';
 	}
 
-	for(int i=0;i< ARRAY_SIZE;i++)
+	for (int i = 0; i < ARRAY_SIZE; i++)
 		for (int j = 0; j < ARRAY_SIZE; j++) {
 			if (input[i][j] == 1 && target[i][j] == 0)
-				move_times += DFS(i, j, 0).times;
+				move_times += DFS(i, j, -1, 1, 3).times;
+			else if (input[i][j] == 0 && target[i][j] == 1)
+				move_times += DFS(i, j, -1, 0, 3).times;
 			else if (input[i][j] == 1 && target[i][j] == 1)
-				target[i][j] = 2;
+				input[i][j] = 2;
 		}
 
 	cout << move_times << endl;
 
 	return 0;
 }
+/*
+0100
+0011
+1100
+0100
 
+1100
+0001
+0011
+1000
+*/
 /* Ëã24 */
 //
+//#include <algorithm> 
+//using std::next_permutation;
+//using std::prev_permutation;
+// 
 //#define ARRAY_SIZE 4
 //
 //bool search(double a[], const int x)
--- 第XV章  友元、异常和其他/Chapter_Fifteen.cpp
+++ 第XV章  友元、异常和其他/Chapter_Fifteen.cpp
@@ -1,6 +1,19 @@
+#include<iostream>
+#include<cstdlib>		// abort()
+#include<cfloat>		// (or float.h) for DBL_MAX
+
+void error1_cpp();
+double hmean(double a, double b);	// abort()测试
+
+void error2_cpp();
+bool hmean(double a, double b, double* ans);	// 返回错误信息测试
+
+void error3_cpp();
+double hmean_throw(double a, double b);		// 异常机制测试
 
 int main()
 {
+	int WhichOne = 0;
 	// 并非只能拥有友元函数,也可以将类作为友元函数
 	// 另外,也可以做更严格的限制,只将特定的成员函数指定为另一个类的友元
 
@@ -126,6 +139,156 @@ int main()
 	//	这使得代码简短且更安全,因为它自动进行初始化,无需程序员记住应该做什么
 	//	如在方法文件中定义构造函数,则定义必须指出 Node 类是在 Queue 类中定义的:
 	//	Queue::Node::Node(const Item & i) : item(i), next(0) {}
-	//	
+	// 
+	// 嵌套类和访问权限
+	//	嵌套类的声明位置确定了嵌套类的作用域
+	//		即它决定了程序的哪些部分可以创建这种类的对象
+	//	其次,和其它类一样,嵌套类的共有部分、保护部分和私有部分控制了对类成员的访问
+	// 
+	//  -作用域
+	//		如果嵌套类是在另一个类的私有部分声明的,则只有后者知道它
+	//		如果嵌套类是在另一个类的保护部分声明的,则它对于后者来说是可见的,但是对外部世界则是不可见的
+	//			然而,在这种情况中,派生类将知道嵌套类,并可以直接创建这种类型的对象
+	//		如果嵌套类是在另一个类的公有部分声明的,则允许后者、后者的派生类以及外部世界去使用它
+	//			然而,由于嵌套类的作用域为包含它的类,因此外部世界使用它是,必须使用类限定符
+	//   嵌套结构和枚举的作用域与此相同
+	//		其实,很多程序员都使用公有枚举来提供可供客户程序员使用的类常数
+	//	-访问控制
+	//		类声明的位置决定了类的作用域或可见性
+	//		类可见后,访问控制规则(公有、保护、私有、友元)将决定程序对嵌套类成员的访问权限
+	// 
+	// 模板中的嵌套
+	//	类QueueTP
+
+	// 异常
+	//	 调用 abort()
+	//		原型位于头文件 cstdlib(或stdlib.h) 中
+	//		其典型实现是向标准错误流(即 cerr 使用的错误流)发送
+	//		消息 abnormal program termination(程序异常终止),然后终止程序
+	//		他还返回一个随时先而异的值,告诉操作系统(如果程序是由另一个程序调用的,则告诉父进程)
+	//		abort()是否刷新文件缓冲区(用于存储读写到文件中的数据的内存区域)取决于实现
+	//		如果愿意,也可以使用 exit(),该函数刷新文件缓冲区,但不显示消息
+	if (WhichOne == 1)error1_cpp();
+	//		注意,在 hmean() 中调用 abort() 函数将直接终止程序,而不是先返回到 main()
+	// 
+	// 返回错误码
+	//		一种比异常终止更灵活的方法是,使用函数的返回值来指出问题
+	//		例如, ostream 类的 get(void) 成员通常返回下一个输入字符的 ASCII 码
+	//		但到达文件尾时,将返回特殊值 EOF
+	//		对 hmean() 来说,这种方法不管用
+	//		任何数值都是有效的返回值,因此不存在可用于指出问题的特殊值
+	//		在这种情况下,可使用指针参数或引用参数来将值返回给调用程序
+	//		并使用函数的返回值来指出成功还是失败
+	//		istream 族重载 >> 运算符使用了这种技术的变体
+	if (WhichOne == 2)error2_cpp();
+		
+	// 异常机制
+	//	异常提供了将控制权从程序的一个部分传递到另一个部分的途径
+	//	对异常的处理有3个组成部分:
+	//		- 引发异常
+	//		- 使用处理程序捕获异常
+	//		- 使用 try 块
+	//			程序出现问题时将引发异常
+	//			如 error3_cpp() 中的 hmean_throw(),使之引发异常而不是调用 abort() 函数
+	//			throw 语句实际上是跳转,即命令程序跳另一条语句
+	//			throw 关键字表示引发异常,紧随其后的值(例如字符串或对象)指出了异常的特征
+	//		程序使用异常处理程序(exception handler)来捕获异常,异常处理程序位于要处理问题的程序中
+	//		catch 关键字表示捕获异常
+	//		处理程序以关键字 catch 开头,随后是位于括号中的类型声明
+	//		它指出异常处理程序要响应的异常类型
+	//		然后是一个用花括号括起来的代码块,指出要采取的措施
+	//		catch 关键字和异常类型用作标签,指出当异常发生时,程序应跳到这个位置执行
+	//		异常处理程序也被称为 catch 块
+	//		try 块标识其中特定的异常可能被激活的代码块,它后面更着一个或多个 catch 块
+	//		try 块是由关键字 try 指示的
+	//		关键字 try 后面是一个由花括号括起的代码块,表明需要注意这些代码引发的异常
+	if (WhichOne == 3)error3_cpp();
+	//		执行 throw 语句类似于执行返回语句,因为它也将终止函数的执行
+	//		但 throw 不是讲控制权返回给调用程序,而是导致程序沿函数调用序列后退
+	//		直到找到包含 try 块的函数
+	//		另外,在这个例子中, throw 将程序控制权返回给 error3_cpp()
+	//		程序将在 error3_cpp() 中寻找与引发的异常类型匹配的异常处理程序(位于 try 块后面)
+	//
 	return 0;
+}
+
+void error1_cpp()
+{
+	double x, y, z;
+	std::cout << "Enter two numbers: ";
+	while (std::cin >> x >> y)
+	{
+		z = hmean(x, y);
+		std::cout << "Harmonic mean of " << x << " and " << y
+			<< " is " << z << std::endl;
+		std::cout << "Enter next set of numbers <q to quit>: ";
+	}
+	std::cout << "Bye!\n";
+}
+double hmean(double a, double b)
+{
+	if (a == -b)
+	{
+		std::cout << "untenable arguments to hmean()\n";
+		std::abort();
+	}
+	return 2.0 * a * b / (a + b);
+}
+
+void error2_cpp()
+{
+	double x, y, z;
+	std::cout << "Enter two numbers: ";
+	while (std::cin >> x >> y)
+	{
+		if (hmean(x, y, &z))
+			std::cout << "Harmonic mean of " << x << " and " << y
+			<< " is " << z << std::endl;
+		else
+			std::cout << "One value should not be the negative "
+			<< "of the other - try again.\n";
+		std::cout << "Enter next set of numbers <q to quit>: ";
+	}
+	std::cout << "Bye!\n";
+}
+bool hmean(double a, double b,double * ans)
+{
+	if (a == -b)
+	{
+		*ans = DBL_MAX;
+		return false;
+	}
+	else {
+		*ans = 2.0 * a * b / (a + b);
+		return true;
+	}
+}
+
+void error3_cpp()
+{
+	double x, y, z;
+
+	std::cout << "Enter two numbers: ";
+	while (std::cin >> x >> y)
+	{
+		try {							// start of try block
+			z = hmean_throw(x, y);
+		}								// end of try block
+		catch (const char* s)			// start of exception handler
+		{
+			std::cout << s << std::endl;
+			std::cout << "Enter a new pair of numbers: ";
+			continue;
+		}								// end of handler
+		std::cout << "Harmonic mean of " << x << " and " << y
+			<< " is " << z << std::endl;
+		std::cout << "Enter next set of numbers <q to quit>: ";
+	}
+	std::cout << "Bye!\n";
+}
+double hmean_throw(double a, double b)
+{
+	if (a = -b)
+		throw"bad hmean() arguments: a = -b not allowed";
+	return 2.0 * a * b / (a + b);
 }
