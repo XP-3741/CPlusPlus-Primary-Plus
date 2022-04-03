22ce1acc22525e55eb5841accf1d6fd5bfbdb663
X_P <2325968853@qq.com>
2022-04-03 23:00:11 +0800

    add

--- 第XVI章 string类和标准模板库/Chapter_Sixteen.cpp
+++ 第XVI章 string类和标准模板库/Chapter_Sixteen.cpp
@@ -2,9 +2,12 @@
 #include<string>
 #include<fstream>
 #include<cstdlib>
+#include <ctime>
+#include <cctype>
 
 void str1_cpp();
 void strfile_cpp();
+void hangman_cpp();
 
 int main()
 {
@@ -108,6 +111,22 @@ int main()
 	//	string 库还提供了相关的方法:
 	//		rfind()、find_first_of()、find_last_of()、find_first_not_of()和find_last_not_of()
 	//		它们的重载函数特征标都与 find()方法相同
+	//		rfind()方法
+	//			查找子字符串或字符最后一次出现的位置
+	//		find_first_of()方法
+	//			在字符串中查找参数中任何一个字符首次出现的位置
+	//			例如下面语句返回 r 再"cobra"中的位置,因为这是"hark"中各个字母在"cobra"首次出现的位置:
+	//			int where = snake1.find_first_of("hark");
+	//		find_last_of()方法
+	//			功能与上相同,只是他查找的是最后一次出现的位置
+	//			因此下面的语句返回 a 在"cobra"中的位置:
+	//			int where = snake1.find_last_of("hark");
+	//		find_first_not_of()方法
+	//			在字符串中查找第一个不包含在参数中的字符
+	//			因此下面的语句返回 c 在"cobra"中的位置,因为"hark"中没有 c:
+	//			int where = snake1.find_last_of("hark");
+	if(false)	hangman_cpp();
+	//	
 	//
 
 	return 0;
@@ -167,4 +186,86 @@ void strfile_cpp()
 	fin.close();
 	// std::cin.get();
 	// std::cin.get();
+}
+
+void hangman_cpp()
+{
+	using std::string;
+	using std::cout;
+	using std::cin;
+	using std::tolower;
+	using std::endl;
+
+	const int NUM = 26;
+	const string wordlist[NUM]= { "apiary", "beetle", "cereal",
+	"danger", "ensign", "florid", "garage", "health", "insult",
+	"jackal", "keeper", "loaner", "manage", "nonce", "onset",
+	"plaid", "quilt", "remote", "stolid", "train", "useful",
+	"valid", "whence", "xenon", "yearn", "zippy" };
+
+	std::srand(std::time(0));
+	char play;
+	cout << "Will you play a word game? <y/n> ";
+	cin >> play;
+	play = tolower(play);
+	while (play == 'y')
+	{
+		string target = wordlist[std::rand() % NUM];
+		int length = target.length();
+		string attempt(length, '-');
+		string badchars;
+		int guesses = 6;
+		cout << "Guess my secret word. It has " << length
+			<< " letters, and you guess\n"
+			<< "one letter at a time. You get " << guesses
+			<< " wrong guesses.\n";
+		cout << "Your word: " << attempt << endl;
+		while (guesses > 0 && attempt != target)
+		{
+			char letter;
+			cout << "Guess a letter: ";
+			cin >> letter;
+			if (badchars.find(letter) != string::npos
+				|| attempt.find(letter) != string::npos)
+			{
+				cout << "You already guessed that. Try again.\n";
+				continue;
+			}
+			int loc = target.find(letter);
+			if (loc == string::npos)
+			{
+				cout << "Oh, bad guess!\n";
+				--guesses;
+				badchars += letter; // add to string
+			}
+			else
+			{
+				cout << "Good guess!\n";
+				attempt[loc] = letter;
+				// check if letter appears again
+				loc = target.find(letter, loc + 1);
+				while (loc != string::npos)
+				{
+					attempt[loc] = letter;
+					loc = target.find(letter, loc + 1);
+				}
+			}
+			cout << "Your word: " << attempt << endl;
+			if (attempt != target)
+			{
+				if (badchars.length() > 0)
+					cout << "Bad choices: " << badchars << endl;
+				cout << guesses << " bad guesses left\n\n";
+			}
+		}
+		if (guesses > 0)
+			cout << "\nThat's right!\n";
+		else
+			cout << "\nSorry, the word is " << target << ".\n";
+
+		cout << "\nWill you play another? <y/n> ";
+		cin >> play;
+		play = tolower(play);
+	}
+	cout << "\nBye\n";
 }
